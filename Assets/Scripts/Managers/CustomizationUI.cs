bf67d666b312e1d823fbc9ca4dd302762f8fc7cb
Mike <mmatthews@manzalab.com>
2021-03-27 14:21:10 +0100

    Base interface panel

---
+++ Assets/Scripts/Managers/CustomizationUI.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>Class used for interractions with the customization panel</summary>
+public class CustomizationUI : BaseBehaviour
+{
+	[Header("Scene references - UI")]
+	public Button exitPanelButton;
+}
