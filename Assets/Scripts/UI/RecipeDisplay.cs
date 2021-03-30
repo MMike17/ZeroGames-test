c3107e7ef39a3fc5ff06974c79704c571e235e11
Mike <mmatthews@manzalab.com>
2021-03-30 19:42:32 +0200

    Clicking a recipe display no longer opens multiple web pages

--- Assets/Scripts/UI/RecipeDisplay.cs
+++ Assets/Scripts/UI/RecipeDisplay.cs
@@ -24,6 +24,7 @@ public class RecipeDisplay : BaseBehaviour
 
 		title.text = recipeTitle;
 
+		openWebSite.onClick.RemoveAllListeners();
 		openWebSite.onClick.AddListener(() => Application.OpenURL(recipe.recipeUrl));
 
 		InitInternal();
